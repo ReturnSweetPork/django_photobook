f5be0127ce4935bf7ae13f6a0831a08765383d49
hyunsang <happylovetkd@naver.com>
2019-02-07 08:54:53 +0000

    DAY24 | 190207 | 장고 사진추가법

---
+++ photo/.~c9_invoke_WGwflc.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Photo
+# Register your models here.
+admin.site.register(Photo)
+admin.site.register(Comment)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
